6f1b02baf16d1e0d5f5951afdeb558b07bf9a9b1
Eric Jones <eric.r.k.jones@gmail.com>
2018-05-25 17:15:02 -0600

    Added functionality to drop high or low dice from a roll.  To use, follow this convention:
    [n]d[x][<(l|h)m>]
    
    where n is the number of dice, x is the number of sides, l|h is either drop low or high dice, and m is the number to drop.
    
    So for example, typical of D&D character generation:
    
    	rolldice 4d6l1
    
    will roll 4d6 and drop the lowest.
    
    Or using the D&D 5e Advantage mechanic:
    
    	rolldice 2d20h1
    
    will roll 2d20 and drop the highest one, as rolling with disadvantage.

--- dice.py
+++ dice.py
@@ -4,20 +4,35 @@ from random import randint as randint
 itermax=1000
 
 def roll(N,sides):
-	return sum([randint(1,sides) for n in range(0,N)])
+	output = [randint(1,sides) for n in range(0,N)]
+	# print(output)
+	return output
 
 def evaldice(matchobj):
-	result = str(roll(int(matchobj.group(1)),int(matchobj.group(2))))
-	# print 'Rolling:', matchobj.group(0), '=', result
-	return result
+	rolled = sorted(roll(int(matchobj.group(1)),int(matchobj.group(2))))
+	if matchobj.group(3) == 'l':
+		result = rolled[int(matchobj.group(4)):]
+		# print "Dropped lowest", matchobj.group(4), "dice:", rolled, '>', result
+	else:
+		if matchobj.group(3) == 'h':
+			result = rolled[:-int(matchobj.group(4))]
+			# print "Dropped highest", matchobj.group(4), "dice:", rolled, '>', result
+		else:
+			result = rolled
+
+
+	total = str(sum(result))
+	# print 'Rolling:', matchobj.groups(), '=', total
+	return total
 
 def evaluatedicelist(matchobj):
 	# print 'inside parentheses -', matchobj.group(0)
-	new = re.sub('([0-9]+)d([0-9]+)', evaldice, matchobj.group(1))
+	# print matchobj.group(1)
+	new = re.sub('([0-9]+)d([0-9]+)(?:(l|h)([0-9]+))?', evaldice, matchobj.group(1))
 	result=str(eval(new))
 	# print 'Evaluating: ', new, '=', result
 	return result
-
+	
 def evaluateparentheses(matchobj):
 	# print 'new level: ', matchobj.group(1)
 	return re.sub('\(([^()]*)\)', evaluatedicelist, matchobj.group(1))
